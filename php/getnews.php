4614c2330ed6d0b60c903a39c7fb893a935338ed
HousseinDroubi <housseinalialdroubi@gmail.com>
2022-09-23 16:48:06 +0300

    Finish html file using Bootstrap 5

--- php/getnews.php
+++ php/getnews.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-include("connection.php");
+include("connection.php");  
 $get_news=$mysqli->prepare("SELECT * FROM news");
 $get_news->execute();
 $array_get_news=$get_news->get_result();
